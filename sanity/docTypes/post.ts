d9c275d65554b655cf79ed392b03b9b355d16324
Scott Wambach <scott@scottwamba.ch>
2024-01-15 12:31:15 -0600

    schema types

--- sanity/docTypes/post.ts
+++ sanity/docTypes/post.ts
@@ -8,5 +8,45 @@ export const post = {
       title: "Title",
       type: "string",
     },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: [{ type: "author" }],
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "category" }] }],
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "array",
+      of: [{ type: "block" }],
+    },
   ],
 };
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { post } from "./docTypes/post";
+import { category } from "./docTypes/category";
+import { author } from "./docTypes/author";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post],
+  types: [post, category, author],
 };
